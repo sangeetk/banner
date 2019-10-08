7c1eb2d8c0495abf25c9480a06b9afbc4ddc802e
Sangeet Kumar <sk@urantiatech.com>
2019-10-08 17:13:51 +0530

    Working example

--- example/handler.go
+++ example/handler.go
@@ -5,5 +5,15 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "images/sample.jpg")
+
+	// Get active banner
+	b, err := Coll.GetActive()
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/jpg")
+	w.Write(b.Image)
+
 }
--- example/main.go
+++ example/main.go
@@ -2,63 +2,110 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 
 	"github.com/sangeetk/banner"
 )
 
+var Coll *banner.Collection
+var startTime time.Time
+
 func main() {
 	fmt.Println("Testing scheduling of banners...")
 
 	// Create a new collection
-	var col = banner.NewCollection("collection1", banner.NewDataStoreMemory())
+	Coll = banner.NewCollection("collection1", banner.NewDataStoreMemory())
 
-	now := time.Now()
+	startTime = time.Now()
 
 	// Time here is in seconds from current time
 	red, _ := banner.NewFile("R", "images/red.jpg")
-	red.AddDuration(now.Add(0*time.Second), now.Add(20*time.Second))
-	col.Add(*red)
-	debugScheduler(&col.Scheduler, now.Unix())
+	red.AddDuration(startTime.Add(0*time.Second), startTime.Add(20*time.Second))
+	Coll.Add(*red)
+	debugScheduler(&Coll.Scheduler, startTime.Unix())
 
 	blue, _ := banner.NewFile("B", "images/blue.jpg")
-	blue.AddDuration(now.Add(40*time.Second), now.Add(60*time.Second))
-	col.Add(*blue)
-	debugScheduler(&col.Scheduler, now.Unix())
+	blue.AddDuration(startTime.Add(40*time.Second), startTime.Add(60*time.Second))
+	Coll.Add(*blue)
+	debugScheduler(&Coll.Scheduler, startTime.Unix())
 
 	green, _ := banner.NewFile("G", "images/green.jpg")
-	green.AddDuration(now.Add(25*time.Second), now.Add(45*time.Second))
-	col.Add(*green)
-	debugScheduler(&col.Scheduler, now.Unix())
+	green.AddDuration(startTime.Add(25*time.Second), startTime.Add(45*time.Second))
+	Coll.Add(*green)
+	debugScheduler(&Coll.Scheduler, startTime.Unix())
 
 	yellow, _ := banner.NewFile("Y", "images/yellow.jpg")
-	yellow.AddDuration(now.Add(70*time.Second), now.Add(100*time.Second))
-	col.Add(*yellow)
-	debugScheduler(&col.Scheduler, now.Unix())
+	yellow.AddDuration(startTime.Add(70*time.Second), startTime.Add(100*time.Second))
+	Coll.Add(*yellow)
+	debugScheduler(&Coll.Scheduler, startTime.Unix())
 
 	// Remove green
-	col.Remove(*green)
-	debugScheduler(&col.Scheduler, now.Unix())
+	Coll.Remove(*green)
+	debugScheduler(&Coll.Scheduler, startTime.Unix())
 
 	orange, _ := banner.NewFile("O", "images/orange.jpg")
-	orange.AddDuration(now.Add(90*time.Second), now.Add(120*time.Second))
-	col.Add(*orange)
-	debugScheduler(&col.Scheduler, now.Unix())
+	orange.AddDuration(startTime.Add(90*time.Second), startTime.Add(120*time.Second))
+	Coll.Add(*orange)
+	debugScheduler(&Coll.Scheduler, startTime.Unix())
 
 	green, _ = banner.NewFile("G", "images/green.jpg")
-	green.AddDuration(now.Add(35*time.Second), now.Add(65*time.Second))
-	col.Add(*green)
-	debugScheduler(&col.Scheduler, now.Unix())
+	green.AddDuration(startTime.Add(35*time.Second), startTime.Add(65*time.Second))
+	Coll.Add(*green)
+	debugScheduler(&Coll.Scheduler, startTime.Unix())
 
 	pink, _ := banner.NewFile("P", "images/pink.jpg")
-	pink.AddDuration(now.Add(120*time.Second), now.Add(130*time.Second))
-	col.Add(*pink)
-	debugScheduler(&col.Scheduler, now.Unix())
+	pink.AddDuration(startTime.Add(120*time.Second), startTime.Add(130*time.Second))
+	Coll.Add(*pink)
+	debugScheduler(&Coll.Scheduler, startTime.Unix())
+
+	mercari, _ := banner.NewFile("M", "images/mercari.jpg")
+	mercari.AddDuration(startTime.Add(0*time.Second), startTime.Add(10*time.Second))
+	Coll.Add(*mercari)
+	debugScheduler(&Coll.Scheduler, startTime.Unix())
+
+	mercari2, _ := banner.NewURL("m", "https://web-jp-assets.mercdn.net/_next/static/images/top-banner-super-exhibition-fes-1354ceda34bd06081a45ee755e911f07.jpg")
+	mercari2.AddDuration(startTime.Add(160*time.Second), startTime.Add(180*time.Second))
+	Coll.Add(*mercari2)
+	debugScheduler(&Coll.Scheduler, startTime.Unix())
 
 	// default banner
 	white, _ := banner.NewFile(".", "images/white.jpg")
-	white.AddDuration(now.Add(0*time.Second), now.Add(160*time.Second))
-	col.Add(*white)
-	debugScheduler(&col.Scheduler, now.Unix())
+	white.AddDuration(startTime.Add(0*time.Second), startTime.Add(160*time.Second))
+	Coll.Add(*white)
+	debugScheduler(&Coll.Scheduler, startTime.Unix())
+
+	// Start http server
+	go func() {
+		http.Handle("/", http.HandlerFunc(Handler))
+		log.Println("Running http server at localhost:8080")
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}()
+
+	// Ask for inputs from user
+	time.Sleep(1 * time.Second)
+
+	for {
+		var sec int64
+		fmt.Print("\nPreview time in sec : ")
+		fmt.Scanf("%v", &sec)
+
+		var active, preview string
+		if b, err := Coll.GetActive(); err != nil {
+			active = "none"
+		} else {
+			active = b.ID
+		}
+
+		t := startTime.Add(time.Duration(sec) * time.Second)
+		if b, err := Coll.Preview(t); err != nil {
+			preview = "none"
+		} else {
+			preview = b.ID
+		}
+
+		fmt.Printf("Time [%v sec] => Active startTime : [%v] , Active at %v sec : [%v]\n", time.Now().Unix()-startTime.Unix(), active, sec, preview)
+	}
 
 }
